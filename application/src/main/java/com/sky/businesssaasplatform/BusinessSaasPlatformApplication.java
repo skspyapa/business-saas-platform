e47088bcc3eddc6ab08da5e22b0207fdef5df684
skspyapa <skspyapa@gmail.com>
2026-04-01 12:51:51 +0530

    initial multi module monorepo

--- application/src/main/java/com/sky/businesssaasplatform/BusinessSaasPlatformApplication.java
+++ application/src/main/java/com/sky/businesssaasplatform/BusinessSaasPlatformApplication.java
@@ -1,4 +1,4 @@
-package com.sky.platform;
+package com.sky.businesssaasplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- application/src/test/java/com/sky/businesssaasplatform/BusinessSaasPlatformApplicationTests.java
+++ application/src/test/java/com/sky/businesssaasplatform/BusinessSaasPlatformApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sky.platform;
+package com.sky.businesssaasplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

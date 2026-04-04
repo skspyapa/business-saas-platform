87727fb2f26afdaa6328f22bdff1d17abe421649
skspyapa <skspyapa@gmail.com>
2026-04-04 23:49:49 +0530

    fix swagger issue

---
+++ application/src/main/java/com/sky/BusinessSaasPlatformApplication.java
@@ -0,0 +1,15 @@
+package com.sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class BusinessSaasPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessSaasPlatformApplication.class, args);
+	}
+
+}
